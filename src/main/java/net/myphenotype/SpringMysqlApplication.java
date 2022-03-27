5236bd66ef0a60223fbcab98e0b76493f5e41e05
Vijay Ponnusamy <jeeves1618@gmail.com>
2022-03-27 20:37:38 +0530

    Initial Version

--- src/main/java/net/myphenotype/SpringMysqlApplication.java
+++ src/main/java/net/myphenotype/SpringMysqlApplication.java
@@ -2,13 +2,14 @@ package net.myphenotype;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @PropertySource("classpath:BookProperties.properties")
-public class SpringMysqlApplication {
+public class SpringMysqlApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMysqlApplication.class, args);
